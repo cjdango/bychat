571fb8c6e3338e2a2c814941fc1a5d4cd2da40eb
cjdango <cjdango@gmail.com>
2017-10-16 13:48:56 +0800

    Generate all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserListComponent,
+    ChatRoomComponent,
+    FeedComponent,
+    MessageComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
